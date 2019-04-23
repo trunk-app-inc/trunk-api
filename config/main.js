c9614e863e4defc9c9f1b5ea5fc6fc7ee96e53ea
Bobby Boyd <bboyd2008@gmail.com>
2019-04-23 15:44:55 -0700

    Changed the database

--- config/main.js
+++ config/main.js
@@ -1,4 +1,4 @@
 module.exports = {
 	'secret': 'r + l = j game of thrones nerds',
-	'database': 'mongodb://trunk-api-mongo:27017/trunk'		//docker requires it mapping to service name
+	'database': 'mongodb://a13jk9jbnk8b:12654fdsf6adf95133354d@ds019708.mlab.com:19708/trunk'		//docker requires it mapping to service name
 }
