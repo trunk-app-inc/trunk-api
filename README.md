a3a8a6887b826fd4f4ac14351b986a814f8cd3bd
robaboyd <bboyd2008@gmail.com>
2019-04-25 15:39:03 -0700

    Reverted Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# trunk-api
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const passport = require('passport')
-// router.use(passport.authenticate('bearer', {session: false}));
+router.use(passport.authenticate('bearer', {session: false}));
 	router.get('/initial', (req, res) => {
 		res.send('api is working');
 	})
